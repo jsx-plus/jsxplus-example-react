e635fcb76358f5387a0e9fa836e5080d40c1ffd3
kaili.lk <kaili.lk@alibaba-inc.com>
2019-08-20 11:41:23 +0800

    test: add cases

--- src/testEslint.jsx
+++ src/testEslint.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const bar = true;
+const foo = true;
+const boo = true;
+
 function WaterFall() {}
 function App() {
   const listData = Array.from({ length: 10 }).map((_, i) => i);
@@ -10,9 +14,31 @@ function App() {
     <div x-memo x-class="App" x-class="root">
       <header x-class="App-header">
         <img src={logo} x-class="App-logo" alt="logo" />
-        <p x-for={(item, index) in listData}>
+
+        {/* x-for loop */}
+        <p x-for={(item, index)in listData}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <p x-for={this.state.listData}>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <p x-for>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        {/* conditions */}
+        <span x-elseif={true}></span>
+        <span x-if={bspanr}></span>
+
+        <span x-elseif={true}></span>
+        <span x-elseif={bar}></span>
+
+        <span x-else={boo}></span>
+
+        <span x-else={foo}></span>
+
         <a
           x-class="App-link"
           href="https://reactjs.org"
@@ -22,6 +48,7 @@ function App() {
           Learn React
         </a>
 
+        {/* x-slot */}
         <WaterFall>
           <view x-slot:header>header1</view>
           <view x-slot:item="props">item: {props.item}</view>
